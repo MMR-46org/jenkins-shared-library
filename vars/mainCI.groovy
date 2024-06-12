930c2fec47827752b636fcff2fe49de1e3483686
MMR-46org <madhanmohanreddyeeda@gmail.com>
2024-06-12 18:42:06 -0500

    adding the node to the mainCI library

---
+++ vars/mainCI.groovy
@@ -0,0 +1,26 @@
+def call () {
+    node ('workstation') {
+        if (env.BRANCH_NAME == 'main') {
+            echo "Nothing to do"
+        }
+        else if (env.BRANCH_NAME ==~ 'PR.*') {
+            common.unitTests()
+            common.integrationTests()
+            common.codeQuality()
+            common.sast()
+            common.secretDetection()
+        }
+        else if(env.TAG_NAME ==~ '.*') {
+            common.sast()
+            common.sca()
+            common.secretDetection()
+            common.artifactProduce()
+
+        }
+        else if(env.BRANCH_NAME ==~ '.*') {
+            common.unitTests()
+            common.integrationTests()
+            common.codeQuality()
+        }
+    }
+}
